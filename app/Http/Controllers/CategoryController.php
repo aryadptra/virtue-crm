dd80bf8bbaa276199cb8bb428d04514d0d16c47d
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-12-01 23:00:50 +0700

    create category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\CategoryRepositoryInterface;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    private CategoryRepositoryInterface $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+}
