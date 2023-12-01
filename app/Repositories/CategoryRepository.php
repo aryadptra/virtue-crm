157330dde93a91dbf54f02ecc36e92aad66dee60
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-12-01 23:00:19 +0700

    create and configure category service

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Models\Category;
+
+class CategoryRepository implements CategoryRepositoryInterface
+{
+    public function getAllCategories()
+    {
+        return Category::all();
+    }
+    public function getCategoryById($categoryId)
+    {
+        return Category::findOrFail($categoryId);
+    }
+    public function deleteCategory($categoryId)
+    {
+        Category::destroy($categoryId);
+    }
+    public function createCategory(array $categoryDetails)
+    {
+        return Category::create($categoryDetails);
+    }
+    public function updateCategory($categoryId, array $newDetails)
+    {
+        return Category::whereId($categoryId)->update($newDetails);
+    }
+}
